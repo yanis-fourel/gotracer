a407b26815f70b16f4c40dd3d3a9d34426872cac
Yanis Fourel <yanis.fourel@ledr.io>
2024-04-07 15:49:15 +0800

    Add fuzzy image in viewport tab

--- main.go
+++ main.go
@@ -11,49 +11,43 @@ import (
 	"fyne.io/fyne/v2/container"
 )
 
-func rgbGradient(x, y, w, h int) color.Color {
-	g := int(float32(x) / float32(w) * float32(255))
-	b := int(float32(y) / float32(h) * float32(255))
-
-	return color.NRGBA{uint8(255 - b), uint8(g), uint8(b), 0xff}
+func makeEditor() fyne.CanvasObject {
+	return container.NewVScroll(container.NewVBox(
+		canvas.NewText("first line of editor", color.Black),
+		canvas.NewText("second line of editor", color.Black),
+	))
 }
 
-func getRandomImage() *canvas.Image {
+func makeViewport() fyne.CanvasObject {
 	img := image.NewRGBA(image.Rect(0, 0, 640, 480))
+	res := canvas.NewImageFromImage(img)
 
-	for x := 0; x < 640; x++ {
-		for y := 0; y < 480; y++ {
-			img.Set(x, y, color.RGBA{
-				R: uint8(rand.Uint32() % 256),
-				G: uint8(rand.Uint32() % 256),
-				B: uint8(rand.Uint32() % 256),
-				A: 255,
-			})
+	go func() {
+		for {
+			for x := 0; x < 640; x++ {
+				for y := 0; y < 480; y++ {
+					img.Set(x, y, color.RGBA{
+						R: uint8(rand.Uint32() % 256),
+						G: uint8(rand.Uint32() % 256),
+						B: uint8(rand.Uint32() % 256),
+						A: 255,
+					})
+				}
+			}
+			res.Refresh()
 		}
-	}
+	}()
 
-	return canvas.NewImageFromImage(img)
-}
-
-func loop(w fyne.Window) {
-	for {
-		w.SetContent(getRandomImage())
-	}
+	return res
 }
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Viewport")
 
-	editor := container.NewVBox(
-		canvas.NewText("first line of editor", color.Black),
-		canvas.NewText("second line of editor", color.Black),
-	)
-	viewport := canvas.NewCircle(color.RGBA{R: 255, G: 126, B: 126, A: 255})
-
 	content := container.NewHSplit(
-		container.NewVScroll(editor),
-		viewport,
+		makeEditor(),
+		makeViewport(),
 	)
 	content.Offset = 0.3
 

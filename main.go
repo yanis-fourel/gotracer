af341b6ec8108a2f235fa78e9e1c93839839c4b0
Yanis Fourel <yanis.fourel@ledr.io>
2024-04-28 17:24:07 +0800

    Cursed terminal drawing

--- main.go
+++ main.go
@@ -1,52 +1,97 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
+	"log"
+	"math/rand"
 
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/muesli/termenv"
 )
 
-func makeEditor(gt *GoTracer) fyne.CanvasObject {
-	return container.NewVScroll(container.NewVBox(
-		canvas.NewText("first line of editor", color.Black),
-		canvas.NewText("second line of editor", color.Black),
-	))
+type Render = struct {
+	img    *image.RGBA
+	update chan bool
 }
 
-func makeViewport(gt *GoTracer) fyne.CanvasObject {
-	gt.render.img = image.NewRGBA(image.Rect(0, 0, 640, 480))
-	render := canvas.NewImageFromImage(gt.render.img)
-	render.FillMode = canvas.ImageFillOriginal
+type Model struct {
+	render Render
+	// scene
+	// camera
+}
 
-	go func() {
-		for {
-			<-gt.render.update
-			render.Refresh()
+func initialModel() *Model {
+	return &Model{
+		render: Render{
+			img:    image.NewRGBA(image.Rect(0, 0, 40, 64)),
+			update: make(chan bool, 1),
+		},
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
 		}
-	}()
+	}
+	return m, tea.ClearScreen
+}
 
-	return render
+func (m Model) View() string {
+	s := ""
+	for x := range m.render.img.Bounds().Dx() {
+		for y := range m.render.img.Bounds().Dy() {
+			s += fmt.Sprintf(
+				"%s%sm ",
+				termenv.CSI,
+				termenv.ANSI256.FromColor(m.render.img.At(x, y)).Sequence(true),
+			)
+			s += " "
+		}
+		s += "\n"
+	}
+	s += fmt.Sprintf(
+		"%s%sm ",
+		termenv.CSI,
+		termenv.ResetSeq,
+	)
+	return s
+}
+
+func (m *Model) Run() {
+	for {
+		for x := 0; x < 640; x++ {
+			for y := 0; y < 480; y++ {
+				m.render.img.Set(x, y, color.RGBA{
+					R: uint8(rand.Uint32() % 256),
+					G: uint8(rand.Uint32() % 256),
+					B: uint8(rand.Uint32() % 256),
+					A: 255,
+				})
+			}
+		}
+		select {
+		case m.render.update <- true:
+		default:
+		}
+	}
 }
 
 func main() {
-	gt := NewGoTracer()
-
-	a := app.New()
-	w := a.NewWindow("Viewport")
-
-	content := container.NewHSplit(
-		makeEditor(gt),
-		makeViewport(gt),
-	)
-	content.Offset = 0.3
-
-	w.SetContent(content)
-
-	go gt.Run()
-
-	w.ShowAndRun()
+	m := initialModel()
+	go m.Run()
+	p := tea.NewProgram(m)
+	_, err := p.Run()
+	if err != nil {
+		log.Fatalf("Alas, there's been an error: %v", err)
+	}
 }
